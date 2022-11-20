e367d988a390102495c4e993140c7290ed814e52
cynthia-1999 <3132308912@qq.com>
2022-11-20 03:19:51 -0800

    update boot.asm to support disk

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -23,29 +23,73 @@ _start:
     ;mov     gs, ax
     ;mov     si, ax
 
-    ;读软盘
-    mov ch, 0   ; 柱面
-    mov dh, 0   ; 磁头
-    mov cl, 2   ; 2扇区
-    mov bx, BOOT_MAIN_ADDR  ;把数据读到0x500的位置
+    ;读硬盘
+    mov ecx, 2  ; 从硬盘哪个扇区开始读
+    mov bl, 2   ; 读取的扇区数量
 
-    mov ah, 0x02 ;读盘操作
-    mov al, 1   ;连续读1个扇区
-    mov dl, 0   ;驱动器编号
+    ; 0x1f2 8bit 指定读取或写入的扇区数
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
 
-    int 0x13    ;使用0x13 BIOS中断读盘
+    ; 0x1f3 8bit iba地址的第八位 0-7
+    inc dx
+    mov al, cl
+    out dx, al
 
-    mov  si, jump_to_setup_msg
-    call print
+    ; 0x1f4 8bit iba地址的中八位 8-15
+    inc dx
+    mov al, ch      ; 取中8位
+    out dx, al
+
+    ; 0x1f5 8bit iba地址的高八位 16-23
+    inc dx
+    shr ecx, 16
+    mov al, cl
+    out dx, al
+
+    ; 0x1f6 8bit
+    ; 0-3 位iba地址的24-27
+    ; 4 0表示主盘 1表示从盘
+    ; 5、7位固定为1
+    ; 6 0表示CHS模式，1表示LAB模式
+    inc dx
+    mov al, ch
+    and al, 0b1110_1111
+    out dx, al
+
+    ; 0x1f7 8bit  命令或状态端口
+    inc dx
+    mov al, 0x20
+    out dx, al
+
+    ; 验证状态
+    ; 3 0表示硬盘未准备好与主机交换数据 1表示准备好了
+    ; 7 0表示硬盘不忙 1表示硬盘忙
+    ; 0 0表示前一条指令正常执行 1表示执行出错 出错信息通过0x1f1端口获得
+.read_check:
+    mov dx, 0x1f7
+    in al, dx
+    and al, 0b10001000  ; 取硬盘状态的第3、7位
+    cmp al, 0b00001000  ; 硬盘数据准备好了且不忙了
+    jnz .read_check
+
+    ; 读数据
+    mov dx, 0x1f0
+    mov cx, 256
+    mov edi, BOOT_MAIN_ADDR
+.read_data:
+    in ax, dx
+    mov [edi], ax
+    add edi, 2
+    loop .read_data
+
+    ; 跳过去
+    mov     si, jump_to_setup_msg
+    call    print
 
     xchg bx, bx
-    jmp  BOOT_MAIN_ADDR
-
-read_floppy_error:
-    mov si, read_floppy_error_msg
-    call print
-    jmp $
-
+    jmp     BOOT_MAIN_ADDR
 ; 如何调用
 ; mov     si, msg   ; 1 传入字符串
 ; call    print     ; 2 调用
@@ -64,14 +108,9 @@ print:
 .done:
     ret
 
-read_floppy_error_msg:
-    db "read floppy error", 10, 13, 0
-
 jump_to_setup_msg:
     db "jump to setup...", 10, 13, 0
 
-msg:
-    db "hello, world", 10, 13, 0
 
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
