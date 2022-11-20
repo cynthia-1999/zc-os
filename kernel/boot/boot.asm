da542eb535981a2e18de4019c2e5ac5ecea321c4
cynthia-1999 <3132308912@qq.com>
2022-11-20 01:40:23 -0800

    add comments to codes

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -8,14 +8,16 @@ _start:
     mov ax, 3
     int 0x10
 
-    mov     ax, 0
-    mov     ss, ax
-    mov     ds, ax
-    mov     es, ax
-    mov     fs, ax
-    mov     gs, ax
-    mov     si, ax
+    ;初始化段寄存器
+    ;mov     ax, 0
+    ;mov     ss, ax
+    ;mov     ds, ax
+    ;mov     es, ax
+    ;mov     fs, ax
+    ;mov     gs, ax
+    ;mov     si, ax
 
+    ;将msg传入si，调用print函数，
     mov     si, msg
     call    print
 
@@ -25,16 +27,16 @@ _start:
 ; mov     si, msg   ; 1 传入字符串
 ; call    print     ; 2 调用
 print:
-    mov ah, 0x0e
-    mov bh, 0
-    mov bl, 0x01
+    mov ah, 0x0e    ;int10中断的设置，ah = 0x0e使显示器像打字机一样的显示字符出来
+    mov bh, 0       ;设置当前的显示页，图形模式为下bh=0
+    mov bl, 0x01    ;图形模式下，设置文字的颜色bl=0x01，文字模式下无作用
 .loop:
-    mov al, [si]
-    cmp al, 0
+    mov al, [si]    ;al存储要显示的字符
+    cmp al, 0       ;如果al是结束符号，就停止输出
     jz .done
-    int 0x10
+    int 0x10        ;调用int10中断打印字符
 
-    inc si
+    inc si          ;继续处理下一个字符
     jmp .loop
 .done:
     ret
@@ -42,5 +44,5 @@ print:
 msg:
     db "hello, world", 10, 13, 0
 
-times 510 - ($ - $$) db 0
-db 0x55, 0xaa
+times 510 - ($ - $$) db 0   ;$表示当前位置，$$表示汇编的开头
+db 0x55, 0xaa       ;填充魔数
